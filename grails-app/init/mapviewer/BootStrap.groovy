90f4ffa1e88a497dd5e16c1af4ae66c611538e9b
sbortman <scott.bortman@gmail.com>
2020-04-24 15:45:06 -0400

    fixed database issue,  moved loader into a services.  suspect it need to be wrapped in transaction

--- grails-app/init/mapviewer/BootStrap.groovy
+++ grails-app/init/mapviewer/BootStrap.groovy
@@ -2,29 +2,10 @@ package mapviewer
 
 class BootStrap
 {
+	CityService cityService
 	
 	def init = { servletContext ->
-		File csvFile = new File( 'cities.csv' )
-		
-		csvFile.eachLine { String line ->
-			List<String> tokens = line.split( ',' )
-			
-			City city = new City(
-				name: tokens[0],
-				country: tokens[1],
-				population: tokens[2]?.toInteger(),
-				capital: tokens[3] == 'Y',
-				longitude: tokens[4]?.toDouble(),
-				latitude: tokens[5].toDouble()
-			)
-			
-			if ( !city.save() )
-			{
-				city.errors.allErrors.each {
-					println it
-				}
-			}
-		}
+		cityService.loadCSV()
 	}
 	
 	def destroy = {
