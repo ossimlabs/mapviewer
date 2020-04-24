cf6552f0eb50b87a52693dcf05e044a9e3b6769d
sbortman <scott.bortman@gmail.com>
2020-04-24 15:31:49 -0400

    rendering shapefiles instead of boxes

--- grails-app/init/mapviewer/BootStrap.groovy
+++ grails-app/init/mapviewer/BootStrap.groovy
@@ -1,9 +1,32 @@
 package mapviewer
 
-class BootStrap {
-
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+class BootStrap
+{
+	
+	def init = { servletContext ->
+		File csvFile = new File( 'cities.csv' )
+		
+		csvFile.eachLine { String line ->
+			List<String> tokens = line.split( ',' )
+			
+			City city = new City(
+				name: tokens[0],
+				country: tokens[1],
+				population: tokens[2]?.toInteger(),
+				capital: tokens[3] == 'Y',
+				longitude: tokens[4]?.toDouble(),
+				latitude: tokens[5].toDouble()
+			)
+			
+			if ( !city.save() )
+			{
+				city.errors.allErrors.each {
+					println it
+				}
+			}
+		}
+	}
+	
+	def destroy = {
+	}
 }
--- grails-app/services/mapviewer/TileRendererService.groovy
+++ grails-app/services/mapviewer/TileRendererService.groovy
@@ -1,9 +1,15 @@
 package mapviewer
 
+import geoscript.geom.Bounds
+import geoscript.layer.Shapefile
+import static geoscript.style.Symbolizers.*
+
 import java.awt.Color
 import java.awt.Graphics2D
 import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
+
+import geoscript.render.Map as GeoScriptMap
 
 class TileRendererService
 {
@@ -13,17 +19,43 @@ class TileRendererService
 		
 		int width = params.WIDTH as int
 		int height = params.HEIGHT as int
+		String srs = params.SRS
+		String bbox = params.BBOX
+		
 		String format = params.FORMAT
-		
-		BufferedImage image = new BufferedImage( width, height, BufferedImage.TYPE_INT_ARGB )
 		ByteArrayOutputStream ostream = new ByteArrayOutputStream()
-		Graphics2D g2d = image.createGraphics()
 		
-		g2d.color = Color.blue
-		g2d.drawRect( 0, 0, width - 1, height - 1 )
-		g2d.dispose()
+		Bounds bounds = new Bounds(
+			*( bbox?.split( ',' )?.collect { it.toDouble() } ),
+			srs
+		)
 		
-		ImageIO.write( image, format?.split( '/' )?.last(), ostream )
+		Shapefile borders = new Shapefile( 'world_adm0.shp' )
+		
+		borders.style = fill( opacity: 0 ) + stroke( color: 'blue' )
+		
+		GeoScriptMap map = new GeoScriptMap(
+			width: width,
+			height: height,
+			bounds: bounds,
+			proj: bounds.proj,
+			layers: [
+				borders
+			]
+		)
+		
+		map.render( ostream )
+		map.close()
+
+//		BufferedImage image = new BufferedImage( width, height, BufferedImage.TYPE_INT_ARGB )
+//		Graphics2D g2d = image.createGraphics()
+//
+//		g2d.color = Color.blue
+//		g2d.drawRect( 0, 0, width - 1, height - 1 )
+//		g2d.dispose()
+//
+//		ImageIO.write( image, format?.split( '/' )?.last(), ostream )
+		
 		
 		[ contentType: format, file: ostream.toByteArray() ]
 	}
