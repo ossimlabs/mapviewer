2d46180fcf6687a01b9d49d688243ace9f44f217
sbortman <scott.bortman@gmail.com>
2020-06-17 11:42:50 -0400

    added skaffold,  doesn't work w/jib at the moment so you'll have to rebuild docker image if you make changes to the source.   changed image name to include our public docker repo.   fixed bug in csv loader to only load data if the table is empty

--- grails-app/services/mapviewer/CsvService.groovy
+++ grails-app/services/mapviewer/CsvService.groovy
@@ -15,29 +15,31 @@ class CsvService
 	// this is where the cities are loaded
 	def loadCSV()
 	{
-		File csvFile = new File( 'data/cities.csv' )
-		
-		csvFile.eachLine { String line ->
-			List<String> tokens = line.split( ',' )
+		if ( City.count() == 0 ) {
+			File csvFile = new File( 'data/cities.csv' )
 			
-			City city = new City(
-				name: tokens[0],
-				country: tokens[1],
-				population: tokens[2]?.toInteger(),
-				capital: tokens[3] == 'Y',
-				longitude: tokens[4]?.toDouble(),
-				latitude: tokens[5].toDouble()
-			)
-			
-			city.location = GeoScript.unwrap( new Point( city.longitude, city.latitude ) )
-			city.location.setSRID( 4326 )
-			
-			if ( !city.save() )
-			{
-				city.errors.allErrors.each {
-					println messageSource.getMessage( it, Locale.default )
+			csvFile.eachLine { String line ->
+				List<String> tokens = line.split( ',' )
+				
+				City city = new City(
+					name: tokens[0],
+					country: tokens[1],
+					population: tokens[2]?.toInteger(),
+					capital: tokens[3] == 'Y',
+					longitude: tokens[4]?.toDouble(),
+					latitude: tokens[5].toDouble()
+				)
+				
+				city.location = GeoScript.unwrap( new Point( city.longitude, city.latitude ) )
+				city.location.setSRID( 4326 )
+				
+				if ( !city.save() )
+				{
+					city.errors.allErrors.each {
+						println messageSource.getMessage( it, Locale.default )
+					}
 				}
 			}
-		}
+		}			
 	}
 }
